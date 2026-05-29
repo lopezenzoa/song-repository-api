4c32da5423d6a0b59b009eb3093a8514368c0895
lopezenzoa <enzoagustinlopez2003@gmail.com>
2026-05-29 12:14:39 -0300

    initial commit

---
+++ src/test/java/com/portfolio/song_repository/SongRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.song_repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
